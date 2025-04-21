32a1551c1842355c161a5a067df7cb0ffddbc474
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-04-21 08:42:03 +0530

    LL

---
+++ linkedList.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class linkedList {
+	public static void main(String[] args) {
+		LinkedList<Integer> l1 = new LinkedList<>();
+
+		l1.add(1);
+		l1.add(3);
+		l1.add(2);
+		l1.add(6);
+		System.out.println(l1);
+
+
+	}
+}
