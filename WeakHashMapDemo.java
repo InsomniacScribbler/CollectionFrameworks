48ebc1c5096a9e55bf3db1ad9186ae3885b572f8
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-05-12 08:08:55 +0530

    WeakHashMapDemo

--- WeakHashMapDemo.java
+++ WeakHashMapDemo.java
@@ -3,10 +3,15 @@ import java.util.WeakHashMap;
 public class WeakHashMapDemo {
 	public static void main(String[] args) {
 		WeakHashMap <String, Image> imageCache = new WeakHashMap<>();
-		imageCache.put("img1", new Image("Image 1"));
-		imageCache.put("img2", new Image("Image 2"));
-		imageCache.put("img3", new Image("Image 3"));
+		String key1 = new String("img1");
+		String key2 = new String("img2");
+
+
+		imageCache.put(key1, new Image("Image 1"));
+		imageCache.put(key2, new Image("Image 2"));
+
 		System.out.println(imageCache);
+		key1 = null; key2 = null;
 		System.gc();
 
 		simulatedAppRunning();
